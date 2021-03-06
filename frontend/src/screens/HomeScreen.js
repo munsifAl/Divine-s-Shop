40c6c8347fa6db0b6e56b9fcebe8e5fceb63dc2c
Munsif Ali <munsifchandio99@gmail.com>
2021-03-06 12:57:20 +0500

    use Redux and thunk for products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,34 @@
-import React,{ useState, useEffect } from 'react'
+import React,{ useEffect } from 'react'
+import { useDispatch ,useSelector  } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../commponents/Product'
-import axios from 'axios'
-
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-     const [products, setProducts] = useState([])
+    
+     const dispatch = useDispatch()
 
-     useEffect(() =>{
-         const fetchProducts = async () => {
-               const{data}= await axios.get('/api/products')
-               setProducts(data)
-         }
-         fetchProducts()
-     },[])
+     const productList = useSelector(state => state.productList)
+     const { loading, error, products } = productList
+
+     useEffect(() => {
+        dispatch(listProducts())
+     }, [dispatch])
      return (
           <>
                <h1>Latest Projects</h1>
-               <Row >
-                    {products.map((product) => (
+              { loading ? <h2> loading....</h2> : error ? <h3>{ error}</h3> : 
+              <Row >
+              {products.map((product) => (
 
-                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                          <Product product={product} />
-                         </Col>
-                    ))}
-                      </Row>
+                   <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product} />
+                   </Col>
+              ))}
+                </Row>
+              
+              }
+               
           </>
      )
 }
